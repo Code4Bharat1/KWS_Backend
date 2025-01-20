fbec3534b1f3f1575ae7470c5fd7abfc4c5362d9
Sanadn12 <ni.sanad1433@gmail.com>
2025-01-20 16:24:00 +0530

    change

--- src/controllers/sandouqchaTransactionController.js
+++ src/controllers/sandouqchaTransactionController.js
@@ -24,6 +24,8 @@ export const getTransactionList = async (req, res) => {
                 first_name: true,
                 last_name: true,
                 email: true,
+                indian_contact_no_1: true,
+                indian_contact_no_2: true,
                 kuwait_contact: true,
                 zone_member: true,
                 kwsid: true,
@@ -33,7 +35,7 @@ export const getTransactionList = async (req, res) => {
               select: {
                 first_name: true,
                 last_name: true,
-                kuwait_contact: true,
+                contact: true,
               },
             },
           },
@@ -74,8 +76,9 @@ export const getTransactionList = async (req, res) => {
       const zone = holderDetails?.zone_member;
 
       const holderContact =
-   
-        holderDetails?.kuwait_contact ||
+      holderDetails?.kuwait_contact ||
+        holderDetails?.indian_contact_no_1 ||
+        holderDetails?.indian_contact_no_2 ||
         "No Contact";
 
       // Enhanced total calculation logic
